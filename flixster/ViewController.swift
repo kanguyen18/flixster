525750023d1528df29ae46482729a89236ce95bf
Khoa Nguyen <kanguyen18@earlham.edu>
2021-02-16 17:16:24 -0500

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by KHOA NGUYEN on 2/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
