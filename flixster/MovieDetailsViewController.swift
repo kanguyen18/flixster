53dfa8423a6d29841ce4896cff389473afc61721
Khoa Nguyen <kanguyen18@earlham.edu>
2021-02-23 21:47:17 -0500

    Assignment 2 final draft

---
+++ flixster/MovieDetailsViewController.swift
@@ -0,0 +1,55 @@
+//
+//  MovieDetailsViewController.swift
+//  flixster
+//
+//  Created by KHOA NGUYEN on 2/23/21.
+//
+
+import UIKit
+import AlamofireImage
+
+class MovieDetailsViewController: UIViewController {
+
+    
+    @IBOutlet weak var backdropView: UIImageView!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLable: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    var movie: [String:Any]!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        titleLable.text = movie["title"] as? String
+        titleLable.sizeToFit()
+        
+        synopsisLabel.text = movie["overview"] as? String
+        synopsisLabel.sizeToFit()
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
+        posterView.af.setImage(withURL: posterUrl!)
+        
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropUrl = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)
+        
+        backdropView.af.setImage(withURL: backdropUrl!)
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by KHOA NGUYEN on 2/23/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
